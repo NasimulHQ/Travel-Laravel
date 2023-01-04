1835b4dcbd1627eb24e5d2e5dd96858cb220bf6b
NasimulHQ <nasimul345@gmail.com>
2023-01-05 02:52:55 +0600

    feat: admin panel redesign

--- resources/views/admin/layout/header.blade.php
+++ resources/views/admin/layout/header.blade.php
@@ -20,28 +20,41 @@
             <ul class="nav navbar-nav">
                 <li class="dropdown user user-menu">
                     <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-                        <img src="/admin-dashboard/dist/img/user2-160x160.jpg" class="user-image" alt="User Image">
-                        <span class="hidden-xs">Alexander Pierce</span>
+                        {{-- <img src="/admin-dashboard/dist/img/user2-160x160.jpg" class="user-image" alt="User Image"> --}}
+                        <span class="hidden-xs">{{ Auth::user()->name }}</span>
                     </a>
                     <ul class="dropdown-menu">
                         <!-- User image -->
                         <li class="user-header">
-                            <img src="/admin-dashboard/dist/img/user2-160x160.jpg" class="img-circle" alt="User Image">
+                            {{-- <img src="/admin-dashboard/dist/img/user2-160x160.jpg" class="img-circle" alt="User Image"> --}}
 
                             <p>
-                                Alexander Pierce - Web Developer 
+                                {{ Auth::user()->name }} - Web Developer 
                             </p>
                         </li>
                         <!-- Menu Body -->
                         
                         <!-- Menu Footer-->
-                        <li class="user-footer">
+                        {{-- <li class="user-footer">
                             <div class="pull-left">
                                 <a href="#" class="btn btn-default btn-flat">Profile</a>
                             </div>
                             <div class="pull-right">
                                 <a href="#" class="btn btn-default btn-flat">Sign out</a>
                             </div>
+                        </li> --}}
+                        <li class="user-footer">
+                            <div class="pull-left">
+                            </div>
+                            <div class="pull-right">
+                                <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
+                                 document.getElementById('logout-form').submit();">
+                                    {{ __('Logout') }}
+                                </a>
+                                
+                                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                    @csrf
+                                </form>
                         </li>
                     </ul>
                 </li>
--- resources/views/admin/layout/sidebar.blade.php
+++ resources/views/admin/layout/sidebar.blade.php
@@ -4,25 +4,13 @@
         <!-- Sidebar user panel -->
         <div class="user-panel">
             <div class="pull-left image">
-                <img src="/admin-dashboard/dist/img/user2-160x160.jpg" class="img-circle" alt="User Image">
+                <img src="/admin-dashboard/dist/img/avatar5.png" class="img-circle" alt="User Image">
             </div>
             <div class="pull-left info">
-                <p>Alexander Pierce</p>
+                <p>{{ Auth::user()->name }}</p>
                 <a href="#"><i class="fa fa-circle text-success"></i> Online</a>
             </div>
         </div>
-        <!-- search form -->
-        {{-- <form action="#" method="get" class="sidebar-form">
-            <div class="input-group">
-                <input type="text" name="q" class="form-control" placeholder="Search...">
-                <span class="input-group-btn">
-                    <button type="submit" name="search" id="search-btn" class="btn btn-flat"><i
-                            class="fa fa-search"></i>
-                    </button>
-                </span>
-            </div>
-        </form> --}}
-        <!-- /.search form -->
         <!-- sidebar menu: : style can be found in sidebar.less -->
         <ul class="sidebar-menu" data-widget="tree">
             <li class="header">MAIN NAVIGATION</li>
@@ -63,7 +51,6 @@
                             Sidebar</a></li>
                 </ul>
             </li> --}}
-
             {{-- <li>
                 <a href="../widgets.html">
                     <i class="fa fa-th"></i> <span>Widgets</span>
@@ -72,7 +59,9 @@
                     </span>
                 </a>
             </li> --}}
-            <li class="treeview">
+
+
+            {{-- <li class="treeview">
                 <a href="#">
                     <i class="fa fa-pie-chart"></i>
                     <span>Charts</span>
@@ -181,13 +170,15 @@
                     </li>
                     <li><a href="#"><i class="fa fa-circle-o"></i> Level One</a></li>
                 </ul>
-            </li>
-            <li><a href="https://adminlte.io/docs"><i class="fa fa-book"></i> <span>Documentation</span></a>
-            </li>
+            </li> --}}
+
             <li class="header">LABELS</li>
+            {{-- <li><a href="https://adminlte.io/docs"><i class="fa fa-book"></i> <span>Documentation</span></a>
+            </li>
+            
             <li><a href="#"><i class="fa fa-circle-o text-red"></i> <span>Important</span></a></li>
             <li><a href="#"><i class="fa fa-circle-o text-yellow"></i> <span>Warning</span></a></li>
-            <li><a href="#"><i class="fa fa-circle-o text-aqua"></i> <span>Information</span></a></li>
+            <li><a href="#"><i class="fa fa-circle-o text-aqua"></i> <span>Information</span></a></li> --}}
         </ul>
     </section>
     <!-- /.sidebar -->
